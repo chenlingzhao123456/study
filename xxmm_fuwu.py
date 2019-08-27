7310170e2c8ac8dbfcaa11d9dd89b2f37a7fe899
chenlingzhao123456 <378813288@corpmail.com>
2019-08-27 18:00:55 +0800

    6

---
+++ xxmm_fuwu.py
@@ -0,0 +1,63 @@
+from tkinter import *
+from socket import *
+from threading import Thread
+
+win = Tk()
+win.title('服务端')    #设置标题
+win.geometry('300x400') #设置窗口大小
+canvas =Canvas(master=win, width=300,height=400,bg='LightYellow') #创建画布 设置窗口背景色
+canvas.pack()
+users = {}      #存客户端信息(用户名 ip port)
+
+
+def run(client_socket):
+    buf_size = 1024 #服务端缓冲区的大小
+    data = client_socket.recv(buf_size)  # 服务端接收数据
+    message_recv=data.decode('utf8')    #数据解码
+    # print(message_recv)   #打印结果是 用户名
+    users[message_recv] = client_socket  # 解码保存信息到字典 user={用户名:客户端消息(ip,port)}
+    text.insert(INSERT,message_recv + '已连接\n')    #在消息框显示是否连接成功
+    while 1:
+        data1 = client_socket.recv(buf_size)   #接收客户端传来的消息
+        data2 = data1.decode('utf8')  #data2打印出来样式  谁谁 : 消息内容
+        info_list = data2.split(':')    #以 : 切分字符串,样式是['谁谁','消息内容']
+        users[info_list[0]].send((message_recv + '说:' + info_list[1]).encode('utf8'))   #把收到的 消息内容 发送出去( 用户名+ 说: 消息内容)
+
+def start():
+    ip = entry_ip.get()  # 获取ip
+    port = entry_port.get()  # 获取端口
+    address = (ip, int(port))   #封装地址
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind(address)  # 绑定ip和端口号
+    server_socket.listen(10)    #开启服务器监听
+    text.insert(INSERT, '服务器启动成功\n')  # 连接消息窗口显示
+    while 1:
+        # 接收客户端的请求
+        client_socket, client_address = server_socket.accept()
+        # 开启一个线程(只要有客户端接入就会开启一个线程)
+        if __name__=='__main__':
+            t = Thread(target=run, args=(client_socket,))
+            t.start()
+
+
+def start_sever():
+    t = Thread(target=start)  # 开启一个线程(任务是开启服务器)
+    t.start()
+
+
+label_ip = Label(master=win, text='ip',bg='Lightpink')
+label_ip.place(x=20, y=20)
+label_port = Label(master=win, text='port',bg='Lightpink')
+label_port.place(x=20, y=50)
+entry_ip = Entry(master=win)
+entry_ip.place(x=60, y=20)
+entry_port = Entry(master=win)
+entry_port.place(x=60, y=50)
+button = Button(master=win, text='启动',bg='LightSkyBlue', height=1, width=36,command=start_sever)       #触发事件
+button.place(x=20, y=80)
+text = Text(master=win, height=5, width=30)
+text.place(x=60, y=130)
+label_text = Label(master=win, text='连接消息',bg='Lightpink')
+label_text.place(x=5, y=150)
+
+win.mainloop()
---
+++ xxmm_kehu1.py
@@ -0,0 +1,72 @@
+from tkinter import *
+from socket import *
+from threading import Thread
+
+win = Tk()
+win.title('客户端')    #设置标题
+win.geometry('330x440') #设置窗口大小
+canvas =Canvas(master=win, width=330,height=440,bg='LightYellow')#创建画布设置背景色
+canvas.pack()
+
+client_socket=None         #空值 储存客户端的信息
+
+def get_info():
+    while 1:
+        buf_size = 1024     #服务端缓冲区的大小
+        data=client_socket.recv(buf_size)      #接收服务器发来的信息
+        text.insert(INSERT,data.decode('utf8'))     #把收到的信息解码 然后显示在信息框上
+
+def connect_server():
+    global client_socket
+    ip=entry_ip.get()       #获取数据
+    port=entry_port.get()
+    user=entry_user.get()
+    client=socket(AF_INET, SOCK_STREAM) #创建socket对象
+    address = (ip, int(port))   #封装地址
+    client.connect(address)      #连接服务器(ip和端口号)
+    client.send(user.encode('utf8'))    #把获取到的消息编码发送给客户端
+    client_socket=client
+    if __name__=='__main__':
+        t=Thread(target=get_info)   #开启线程(任务)
+        t.start()
+
+def send_message(): #发消息
+    send_who=entry_send_who.get()   #获取 发给谁 框里的内容
+    send_msg=text_send.get('0.0','end') #获取要发的消息  就是显示消息 框里的内容(索引 浮点型)
+    send_msg=send_who+':'+send_msg  #把消息重新赋值 格式 (谁:消息)
+    client_socket.send(send_msg.encode('utf8'))  #编码发送
+
+label_user = Label(master=win, text='用户名',bg='Lightpink')
+label_user.place(x=20, y=20)
+entry_user = Entry(master=win)
+entry_user.place(x=70, y=20)
+label_ip=Label(master=win,text='ip',bg='Lightpink')
+label_ip.place(x=20,y=50)
+entry_ip=Entry(master=win)
+entry_ip.place(x=70,y=50)
+label_port=Label(master=win,text='port',bg='Lightpink')
+label_port.place(x=20,y=80)
+entry_port=Entry(master=win)
+entry_port.place(x=70,y=80)
+button=Button(master=win,text='启动',bg='LightSkyBlue',height=1,width=40,command=connect_server)  #触发一个事件
+button.place(x=20,y=110)
+
+text=Text(master=win,height=5,width=30)
+text.place(x=80,y=150)
+label_text=Label(master=win,text='显示消息',bg='Lightpink')
+label_text.place(x=20,y=170)
+
+label_send=Label(master=win,text='发送消息',bg='Lightpink')
+label_send.place(x=20,y=250)
+text_send=Text(master=win,height=3,width=30)
+text_send.place(x=80,y=240)
+
+label_send_who=Label(master=win,text='发给谁',bg='Lightpink')
+label_send_who.place(x=20,y=300)
+entry_send_who=Entry(master=win)
+entry_send_who.place(x=80,y=300)
+
+button1=Button(master=win,text='发送',bg='LightSkyBlue',height=1,width=40,command=send_message)   #触发事件
+button1.place(x=20,y=340)
+
+win.mainloop()
